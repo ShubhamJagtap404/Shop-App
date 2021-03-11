1554d1d630c8c2331cf49d6dd581e26cf0085515
ShubhamJagtap404 <51310448+ShubhamJagtap404@users.noreply.github.com>
2021-03-11 12:52:54 +0530

    Handle Authetication Errors and display the home screen as products_overview_screen if a valid token is present else display auth-screen for an invalid token.

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,25 +1,57 @@
 import 'dart:async';
 
 import 'package:flutter/widgets.dart';
+import '../models/http_exception.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class Auth with ChangeNotifier {
   String _token;
   DateTime _expiryDate;
-  String userId;
+  String _userId;
 
-  Future<void> _authenticate(String email, String password, String authType) async {
-    final url = "https://identitytoolkit.googleapis.com/v1/accounts:$authType?key=AIzaSyALmlmcnlsQT0QaFBEYHL3zoZyONUpIAc4";
-    final response = await http.post(url, body: json.encode({
-      'email': email,
-      'password': password,
-      'returnSecureToken': true,
-    }));
-    print(json.decode(response.body));
+  bool get isAuth {
+    return token != null;
   }
 
-  Future<void> signup(String email, String password) async{
+  String get token {
+    if (_expiryDate != null &&
+        _expiryDate.isAfter(DateTime.now()) &&
+        _token != null) {
+      return _token;
+    }
+    return null;
+  }
+
+  Future<void> _authenticate(
+      String email, String password, String authType) async {
+    final url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:$authType?key=AIzaSyALmlmcnlsQT0QaFBEYHL3zoZyONUpIAc4";
+    try {
+      final response = await http.post(url,
+          body: json.encode({
+            'email': email,
+            'password': password,
+            'returnSecureToken': true,
+          }));
+      final responseBody = json.decode(response.body);
+      if (responseBody.containsKey('error') && responseBody['error'] != null) {
+        throw HttpException(responseBody['error']['message']);
+      }
+      _token = responseBody['idToken'];
+      _userId = responseBody['localId'];
+      _expiryDate = DateTime.now().add(
+        Duration(
+          seconds: int.parse(responseBody['expiresIn']),
+        ),
+      );
+      notifyListeners();
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  Future<void> signup(String email, String password) async {
     return _authenticate(email, password, "signUp");
   }
 
