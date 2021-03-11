cca4af798b0c0d6f68d1a8ee34ccfb1f9a084b15
ShubhamJagtap404 <51310448+ShubhamJagtap404@users.noreply.github.com>
2021-03-11 13:10:19 +0530

    Fix a debug in auth file

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -26,7 +26,7 @@ class Auth with ChangeNotifier {
   Future<void> _authenticate(
       String email, String password, String authType) async {
     final url =
-        "https://identitytoolkit.googleapis.com/v1/accounts:$authType?key=AIzaSyALmlmcnlsQT0QaFBEYHL3zoZyONUpIAc4";
+        "https://identitytoolkit.googleapis.com/v1/accounts:$authType?key=[API_KEY]";
     try {
       final response = await http.post(url,
           body: json.encode({
